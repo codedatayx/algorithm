34c2110052023266ddda3fad49e70272f65cf478
codedatayx <110516112+codedatayx@users.noreply.github.com>
2025-11-30 01:10:07 +0800

    Update 最大子数组和.java

--- 最大子数组和.java
+++ 最大子数组和.java
@@ -19,6 +19,7 @@ class Solution {
         int ans = dp[0];
         for(int i=1;i<nums.length;i++){
             dp[i] = Math.max(dp[i-1],0)+nums[i];
+            //dp[i] = Math.max(dp[i-1]+num[i],nums[i]])
             ans = Math.max(ans,dp[i]);
         }       
         return ans; 
