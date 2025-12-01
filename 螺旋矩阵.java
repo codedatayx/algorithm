31bfa79f2a6c7116386a649d91eeedd2c7d927d2
codedatayx <110516112+codedatayx@users.noreply.github.com>
2025-12-01 14:25:32 +0800

    Update 螺旋矩阵.java

--- 螺旋矩阵.java
+++ 螺旋矩阵.java
@@ -19,23 +19,5 @@ class Solution {
         return s1;
     }
 }
-//方法1
-//重点是用一个矩阵来模拟方向
-//用di来表示转弯是重点
-//方法2 ，直接旋转，可以得到状态转移方程
-matrix_new[j][n - i - 1] = matrix[i][j];
-class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n / 2; ++i) {
-            for (int j = 0; j < (n + 1) / 2; ++j) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n - j - 1][i];
-                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
-                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
-                matrix[j][n - i - 1] = temp;
-            }
-        }
-    }
-}
-//分块旋转
+
+
