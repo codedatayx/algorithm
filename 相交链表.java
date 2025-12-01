ddc23b5d9bbe2a3fcf9eb7d6c9dc8c2b4fc016af
codedatayx <110516112+codedatayx@users.noreply.github.com>
2025-12-01 15:18:45 +0800

    Update 相交链表.java

--- 相交链表.java
+++ 相交链表.java
@@ -15,3 +15,28 @@ public class Solution {
         return null;
     }
 }
+//链表的相交指的是在后续所有的节点都一样
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null||headB == null){
+            return null;
+        }
+        ListNode pa = headA ,pb = headB;
+        while(pa!=pb){
+            pa = pa == null ? headB:pa.next;
+            pb = pb == null ? headA:pb.next;
+        }
+        return pa;
+    }
+}
