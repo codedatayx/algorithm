9cebd0ff69a268e25f78f1e6a97d042a4b370e14
codedatayx <110516112+codedatayx@users.noreply.github.com>
2026-01-11 16:51:12 +0800

    Update 缺失的第一个正数.java

--- 缺失的第一个正数.java
+++ 缺失的第一个正数.java
@@ -12,3 +12,23 @@ class Solution {
     }
 }
 //解法1，复杂度太高
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            while(nums[i]<=n&&nums[i]>=1&&nums[i]!=nums[nums[i]-1]){
+                int t = nums[i]-1;
+                int temp = nums[t];
+                nums[t] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
+//找座位，对于i来说，他的座位应该是i-1，如果不是则缺少
